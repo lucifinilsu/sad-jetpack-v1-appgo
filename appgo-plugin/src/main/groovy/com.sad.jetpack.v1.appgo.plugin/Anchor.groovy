87d237f994acf8215e25835a63768f718258d254
lucifinilsu <lucifinilsu@gmail.com>
2022-10-28 18:23:22 +0800

    plugin 修正AppGo被调用时的包名

--- appgo-plugin/src/main/groovy/com.sad.jetpack.v1.appgo.plugin/Anchor.groovy
+++ appgo-plugin/src/main/groovy/com.sad.jetpack.v1.appgo.plugin/Anchor.groovy
@@ -69,7 +69,7 @@ class Anchor {
 
     private static void appInitContext(Project project,StringBuilder defaultCode){
         StringBuffer initContextCode=new StringBuffer()
-        initContextCode.append("com.sad.jetpack.architecture.appgo.api.AppGo.init(this);")
+        initContextCode.append("com.sad.jetpack.v1.appgo.plugin.AppGo.init(this);")
         defaultCode.append(initContextCode.toString())
     }
 
