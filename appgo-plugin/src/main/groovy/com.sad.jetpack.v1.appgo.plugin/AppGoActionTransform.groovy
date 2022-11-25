4cf8ccbfaf6b9e62df65b64d6f69b6efff58bdc7
lucifinilsu <lucifinilsu@gmail.com>
2022-11-25 16:22:16 +0800

    plugin 0.1.8 compileSdk 32 否则33会导致xml布局文件代码提示。

--- appgo-plugin/src/main/groovy/com.sad.jetpack.v1.appgo.plugin/AppGoActionTransform.groovy
+++ appgo-plugin/src/main/groovy/com.sad.jetpack.v1.appgo.plugin/AppGoActionTransform.groovy
@@ -369,7 +369,6 @@ class AppGoActionTransform extends Transform implements ClassScanner.OnFileScann
         anchorCode=ps.toString()
         if (anchorCode!=null && !"".equals(anchorCode.toString())){
             CtMethod method=Anchor.getOverwirteMethodFromApplication(project,scanResult.applicationClass,overwriteApplicationMethodName,anchorMethodParametersClass)
-
             setAnchor(scanResult,method,anchorCode,!anchorMethodName.contains("Pre"))
         }
     }
